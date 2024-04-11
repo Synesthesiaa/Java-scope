6f15985b522b0878612b8e06da141c810a6633d5
J <106944643+Synesthesiaa@users.noreply.github.com>
2024-04-11 16:16:06 +0800

    Create Factorial.java

---
+++ Factorial.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class MikeJava {
+  
+    public static int factorial(int n) {
+    if (n == 0) {
+      return 1;
+     }
+    else{
+      return n  * factorial(n - 1 );            
+     }
+    }
+    public static void main(String[] args) {
+        Scanner scan = new Scanner (System.in);
+        
+        System.out.println("Enter Number: ");
+        int num = scan.nextInt();
+        
+        int result = factorial(num);
+        System.out.println("Factorial of " + num + " is " + result);
+     }
+        }
